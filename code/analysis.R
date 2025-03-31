95b13a380690313b7540a421ab984e8f01be2f3c
Davi Alves Oliveira <davi-ao@users.noreply.github.com>
2025-03-31 11:37:50 -0300

    1.0.0 - Results

--- code/analysis.R
+++ code/analysis.R
@@ -1,37 +1,73 @@
 library(tidyverse)
 library(openxlsx)
 
-files = list.files('./analyses/', pattern = '*_descriptives.xlsx')
+files_slate = list.files('./results/journal_slate/', 
+                   pattern = '*_cohesion_indices.xlsx')
 
-descriptives = lapply(files, function(f) {
-  read.xlsx(paste0('./analyses/', f)) %>%
-    pivot_wider(names_from = 'Variable', values_from = 'Value') %>%
-    mutate(text = f)
+files_icic = list.files('./results/letters_icic/', 
+                         pattern = '*_cohesion_indices.xlsx')
+
+files_pg = list.files('./results/short_stories/', 
+                        pattern = '*_cohesion_indices.xlsx')
+
+files_biomed = list.files('./results/technical_biomed/', 
+                        pattern = '*_cohesion_indices.xlsx')
+
+files_gov = list.files('./results/technical_government_media/', 
+                        pattern = '*_cohesion_indices.xlsx')
+
+files_plos = list.files('./results/technical_plos/', 
+                        pattern = '*_cohesion_indices.xlsx')
+
+indices_slate = lapply(files_slate, function(f) {
+  read.xlsx(paste0('./results/journal_slate/', f)) %>%
+    mutate(Text = f,
+           Genre = 'slate')
 }) %>%
   bind_rows()
 
-descriptives %>%
-  # mutate(text = text %>% str_sub(8, 16) %>%
-  #          str_replace_all('247_', '0') %>%
-  #          str_replace_all('IP_', '1') %>%
-  #          str_remove_all('_') %>%
-  #          str_remove_all('d') %>%
-  #          as.numeric()) %>%
-  arrange(text) %>%
-  write.xlsx('./analyses/descriptives.xlsx')
-
-files = list.files('./analyses/', pattern = '*_cohesion_indices.xlsx')
-
-indices = lapply(files, function(f) {
-  read.xlsx(paste0('./analyses/', f)) %>%
-    mutate(Text = f)
+indices_icic = lapply(files_icic, function(f) {
+  read.xlsx(paste0('./results/letters_icic/', f)) %>%
+    mutate(Text = f,
+           Genre = 'icic')
 }) %>%
+  bind_rows()
+
+indices_pg = lapply(files_pg, function(f) {
+  read.xlsx(paste0('./results/short_stories/', f)) %>%
+    mutate(Text = f,
+           Genre = 'pg')
+}) %>%
+  bind_rows()
+
+indices_biomed = lapply(files_biomed, function(f) {
+  read.xlsx(paste0('./results/technical_biomed/', f)) %>%
+    mutate(Text = f,
+           Genre = 'biomed')
+}) %>%
+  bind_rows()
+
+indices_gov = lapply(files_gov, function(f) {
+  read.xlsx(paste0('./results/technical_government_media/', f)) %>%
+    mutate(Text = f,
+           Genre = 'gov')
+}) %>%
+  bind_rows()
+
+indices_plos = lapply(files_plos, function(f) {
+  read.xlsx(paste0('./results/technical_plos/', f)) %>%
+    mutate(Text = f,
+           Genre = 'plos')
+}) %>%
+  bind_rows()
+
+indices = list(indices_biomed,
+               indices_gov,
+               indices_icic,
+               indices_pg,
+               indices_plos,
+               indices_slate) %>%
   bind_rows()
 
 indices %>%
-  pivot_longer(c(Vertex, Edge), names_to = 'Type', values_to = 'Value') %>%
-  group_by(Text, Type, Index) %>%
-  summarise(Mean = mean(Value)) %>%
-  ggplot(aes(Type, Mean, fill = Index)) +
-  geom_boxplot() +
-  geom_jitter()
+  write.xlsx('./results/experiment4_cohesion_indices.xlsx')
